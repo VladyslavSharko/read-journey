0f5a495ee2d888f05d55c3d44e7ecb2ea8d11c54
Vladlen8432 <127045075+Vladlen8432@users.noreply.github.com>
2025-02-23 21:44:14 +0200

    Test

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,6 @@ import { useDispatch } from "react-redux";
 import { useEffect } from "react";
 import { getMeThunk } from "./redux/auth/operations";
 import PrivateRoute from "./Routes/PrivateRoute";
-import PublicRoute from "./Routes/PublicRoute";
 
 const App = () => {
   const dispatch = useDispatch();
@@ -21,10 +20,8 @@ const App = () => {
   return (
     <div className="appContainer">
       <Routes>
-        <Route element={<PublicRoute />}>
           <Route path="/register" element={<RegisterPage />} />
           <Route path="/login" element={<LoginPage />} />
-        </Route>
 
         <Route element={<PrivateRoute />}>
           <Route path="/" element={<Layout />}>
