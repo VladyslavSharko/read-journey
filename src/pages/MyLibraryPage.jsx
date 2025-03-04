73fb1f712b9dd17ea4f340f5a9be9482d4943be8
Vladlen8432 <127045075+Vladlen8432@users.noreply.github.com>
2025-03-04 12:34:07 +0200

    Add My Library for desktop

--- src/pages/MyLibraryPage.jsx
+++ src/pages/MyLibraryPage.jsx
@@ -1,11 +1,14 @@
 import Dashboard from "../components/Dashboard/Dashboard";
 import MyLibrary from "../components/MyLibrary/MyLibrary";
+import "./styles.css";
 
 const MyLibraryPage = () => {
-  return <div>
-    <Dashboard/>
-    <MyLibrary/>
-  </div>;
+  return (
+    <div className="myLibraryPage">
+      <Dashboard />
+      <MyLibrary />
+    </div>
+  );
 };
 
 export default MyLibraryPage;
