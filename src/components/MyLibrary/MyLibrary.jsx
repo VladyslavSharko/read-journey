fef73eb5581e17d1f17006ebe5a3d9ec9a53e42a
Vladlen8432 <127045075+Vladlen8432@users.noreply.github.com>
2025-03-08 20:19:04 +0200

    Fix elements

--- src/components/MyLibrary/MyLibrary.jsx
+++ src/components/MyLibrary/MyLibrary.jsx
@@ -12,17 +12,11 @@ const MyLibrary = () => {
 
   const removeBook = (id) => {
     if (id) {
-      console.log(`Deleting book with ID: ${id}`);
       dispatch(removeFromLibrary({ id }));
     } else {
       console.error("Book ID is undefined!");
     }
   };
-
-  // const removeBook = (bookId) => {
-  //   console.log("Deleting book with ID:", bookId);
-  //   dispatch(removeFromLibrary(bookId));
-  // };
 
   return (
     <div className={css.myLibrary}>
@@ -73,47 +67,3 @@ const MyLibrary = () => {
 };
 
 export default MyLibrary;
-
-// import { Dropdown } from "../Dropdown/Dropdown";
-// import books from "../../assets/images/books@2x.png";
-// import css from "./MyLibrary.module.css";
-// import { DeleteBtnIcon } from "../Icons";
-
-// const MyLibrary = () => {
-//   return (
-//     <div className={css.myLibrary}>
-//       <div className={css.containerTitleDropdown}>
-//         <h3 className={css.myLibraryTitle}>My library</h3>
-//         <Dropdown />
-//       </div>
-
-//       <div className={css.emptyMyLibrary}>
-//         <div className={css.booksImgWrapper}>
-//           <img className={css.booksimg} src={books} alt="Books" />
-//         </div>
-//         <p className={css.emptyLibraryText}>
-//           To start training, add{" "}
-//           <span className={css.libraryTextSpan}>some of your books</span> or
-//           from the recommended ones
-//         </p>
-//       </div>
-
-//       <ul className={css.addedBooksList}>
-//         <li className={css.addedBooksItem}>
-//           <img className={css.bookImg} src="" alt="Book" />
-//           <div className={css.containerBookInfo}>
-//             <div className={css.subcontainerBookInfo}>
-//               <h3 className={css.bookInfoTitle}>Book Title</h3>
-//               <p className={css.bookInfoAuthor}>Book Author</p>
-//             </div>
-//             <button type="button" className={css.deleteBtnContainer}>
-//               <DeleteBtnIcon />
-//             </button>
-//           </div>
-//         </li>
-//       </ul>
-//     </div>
-//   );
-// };
-
-// export default MyLibrary;
--- src/redux/books/slice.js
+++ src/redux/books/slice.js
@@ -43,7 +43,7 @@ const booksSlice = createSlice({
       })
       .addCase(removeFromLibrary.fulfilled, (state, action) => {
         state.library = state.library.filter(
-          (book) => book.id !== action.payload.id
+          (book) => book._id !== action.payload._id
         );
       })
       .addCase(removeFromLibrary.rejected, (state, action) => {
@@ -65,55 +65,3 @@ export const selectFilteredBooks = createSelector(
 
 export const { setFilter } = booksSlice.actions;
 export const booksReducer = booksSlice.reducer;
-
-// import { createSelector, createSlice } from "@reduxjs/toolkit";
-// import { fetchRecommendedBooks } from "./operations";
-
-// const initialState = {
-//   items: [],
-//   isLoading: false,
-//   error: null,
-//   filter: {
-//     title: "",
-//     author: "",
-//   },
-// };
-
-// const booksSlice = createSlice({
-//   name: "books",
-//   initialState,
-//   reducers: {
-//     setFilter(state, action) {
-//       state.filter = action.payload;
-//     },
-//   },
-//   extraReducers: (builder) => {
-//     builder
-//       .addCase(fetchRecommendedBooks.pending, (state) => {
-//         state.isLoading = true;
-//         state.error = null;
-//       })
-//       .addCase(fetchRecommendedBooks.fulfilled, (state, action) => {
-//         state.isLoading = false;
-//         state.items = action.payload.results;
-//       })
-//       .addCase(fetchRecommendedBooks.rejected, (state, action) => {
-//         state.isLoading = false;
-//         state.error = action.payload;
-//       });
-//   },
-// });
-
-// export const selectFilteredBooks = createSelector(
-//   [(state) => state.books.items, (state) => state.books.filter],
-//   (books, filter) => {
-//     return books.filter(
-//       (book) =>
-//         book.title.toLowerCase().includes(filter.title.toLowerCase()) &&
-//         book.author.toLowerCase().includes(filter.author.toLowerCase())
-//     );
-//   }
-// );
-
-// export const { setFilter } = booksSlice.actions;
-// export const booksReducer = booksSlice.reducer;
