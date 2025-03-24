6c8fc7dd940e065d63529822e34920d9109324a8
Vladlen8432 <127045075+Vladlen8432@users.noreply.github.com>
2025-03-24 16:29:22 +0200

    Test

--- src/components/MyReading/MyReading.jsx
+++ src/components/MyReading/MyReading.jsx
@@ -1,27 +1,10 @@
 import { useLocation } from "react-router-dom";
 import { RecordIcon } from "../Icons";
 import css from "./MyReading.module.css";
-import { useDispatch } from "react-redux";
-import { finishReadingBook, startReadingBook } from "../../redux/books/operations";
 
 const MyReading = () => {
   const location = useLocation();
-  const dispatch = useDispatch();
   const book = location.state?.book;
-
-  const handleStartReading = () => {
-    if (book) {
-      console.log("Starting reading:", { id: book._id, status: book.status });
-      dispatch(startReadingBook({ id: book._id, page: 1 }));
-    }
-  };
-
-  const handleStopReading = () => {
-    if (book) {
-      console.log("Stopping reading:", { id: book._id, status: book.status });
-      dispatch(finishReadingBook({ id: book._id, page: 1 }));
-    }
-  };
 
   return (
     <div className={css.myReading}>
@@ -32,14 +15,9 @@ const MyReading = () => {
           <img className={css.bookImg} src={book.imageUrl} alt={book.title} />
           <h4 className={css.bookTitle}>{book.title}</h4>
           <p className={css.bookAuthor}>{book.author}</p>
-          <button
-            className={css.startButton}
-            type="button"
-            onClick={handleStartReading}
-          >
+          <button className={css.startButton} type="button">
             <RecordIcon className={css.startIcon} />
           </button>
-          <button type="button" onClick={handleStopReading}>Stop reading</button>
         </div>
       ) : (
         <p className={css.noBook}>No book selected for reading.</p>
@@ -49,33 +27,3 @@ const MyReading = () => {
 };
 
 export default MyReading;
-
-// import { useLocation } from "react-router-dom";
-// import { RecordIcon } from "../Icons";
-// import css from "./MyReading.module.css";
-
-// const MyReading = () => {
-//   const location = useLocation();
-//   const book = location.state?.book;
-
-//   return (
-//     <div className={css.myReading}>
-//       <h3 className={css.myReadingTitle}>My reading</h3>
-
-//       {book ? (
-//         <div className={css.bookContainer}>
-//           <img className={css.bookImg} src={book.imageUrl} alt={book.title} />
-//           <h4 className={css.bookTitle}>{book.title}</h4>
-//           <p className={css.bookAuthor}>{book.author}</p>
-//           <button className={css.startButton} type="button">
-//             <RecordIcon className={css.startIcon} />
-//           </button>
-//         </div>
-//       ) : (
-//         <p className={css.noBook}>No book selected for reading.</p>
-//       )}
-//     </div>
-//   );
-// };
-
-// export default MyReading;
--- src/redux/books/slice.js
+++ src/redux/books/slice.js
@@ -3,8 +3,6 @@ import {
   fetchRecommendedBooks,
   addToLibrary,
   removeFromLibrary,
-  startReadingBook,
-  finishReadingBook,
 } from "./operations";
 
 const initialState = {
@@ -50,43 +48,6 @@ const booksSlice = createSlice({
       })
       .addCase(removeFromLibrary.rejected, (state, action) => {
         state.error = action.payload;
-      })
-      .addCase(startReadingBook.pending, (state) => {
-        state.isLoading = true;
-        state.error = null;
-      })
-      .addCase(startReadingBook.fulfilled, (state, action) => {
-        state.isLoading = false;
-        const index = state.items.findIndex(
-          (book) => book._id === action.payload._id
-        );
-        if (index !== -1) {
-          state.items[index] = {
-            ...state.items[index],
-            currentPage: action.payload.page,
-            isReading: true,
-          };
-        }
-      })
-      .addCase(startReadingBook.rejected, (state, action) => {
-        state.isLoading = false;
-        state.error = action.payload;
-      })
-      .addCase(finishReadingBook.pending, (state) => {
-        state.isLoading = true;
-        state.error = null;
-      })
-      .addCase(finishReadingBook.fulfilled, (state, action) => {
-        const index = state.items.findIndex(
-          (book) => book._id === action.payload._id
-        );
-        if (index !== -1) {
-          state.items[index] = {
-            ...state.items[index],
-            currentPage: action.payload.page,
-            isReading: false,
-          };
-        }
       });
   },
 });
@@ -104,71 +65,3 @@ export const selectFilteredBooks = createSelector(
 
 export const { setFilter } = booksSlice.actions;
 export const booksReducer = booksSlice.reducer;
-
-// import { createSelector, createSlice } from "@reduxjs/toolkit";
-// import {
-//   fetchRecommendedBooks,
-//   addToLibrary,
-//   removeFromLibrary,
-// } from "./operations";
-
-// const initialState = {
-//   items: [],
-//   library: [],
-//   isLoading: false,
-//   error: null,
-//   filter: {
-//     title: "",
-//     author: "",
-//   },
-// };
-
-// const booksSlice = createSlice({
-//   name: "books",
-//   initialState,
-//   reducers: {
-//     setFilter(state, action) {
-//       state.filter = action.payload;
-//     },
-//   },
-//   extraReducers: (builder) => {
-//     builder
-//       .addCase(fetchRecommendedBooks.pending, (state) => {
-//         state.isLoading = true;
-//         state.error = null;
-//       })
-//       .addCase(fetchRecommendedBooks.fulfilled, (state, action) => {
-//         state.isLoading = false;
-//         state.items = action.payload.results;
-//       })
-//       .addCase(fetchRecommendedBooks.rejected, (state, action) => {
-//         state.isLoading = false;
-//         state.error = action.payload;
-//       })
-//       .addCase(addToLibrary.fulfilled, (state, action) => {
-//         state.library.push(action.payload);
-//       })
-//       .addCase(removeFromLibrary.fulfilled, (state, action) => {
-//         state.library = state.library.filter(
-//           (book) => book._id !== action.payload._id
-//         );
-//       })
-//       .addCase(removeFromLibrary.rejected, (state, action) => {
-//         state.error = action.payload;
-//       });
-//   },
-// });
-
-// export const selectFilteredBooks = createSelector(
-//   [(state) => state.books.items, (state) => state.books.filter],
-//   (books, filter) => {
-//     return books.filter(
-//       (book) =>
-//         book.title.toLowerCase().includes(filter.title.toLowerCase()) &&
-//         book.author.toLowerCase().includes(filter.author.toLowerCase())
-//     );
-//   }
-// );
-
-// export const { setFilter } = booksSlice.actions;
-// export const booksReducer = booksSlice.reducer;
