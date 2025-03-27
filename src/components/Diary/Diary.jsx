f9c9a6e559017f76a7365e979bc779f39d9d1f50
Vladlen8432 <127045075+Vladlen8432@users.noreply.github.com>
2025-03-27 15:57:00 +0200

    Add Diary for mobile

--- src/components/Diary/Diary.jsx
+++ src/components/Diary/Diary.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import {
   DeleteBtnIcon,
   HourglassIcon,
@@ -7,45 +8,101 @@ import {
 import css from "./Diary.module.css";
 
 const Diary = () => {
+  const [activeTab, setActiveTab] = useState("diary");
+
   return (
     <div className={css.diary}>
       <div className={css.containerTitle}>
-        <h3 className={css.diaryTitle}>Diary</h3>
+        <h3 className={css.title}>
+          {activeTab === "diary" ? "Diary" : "Statistics"}
+        </h3>
 
         <div className={css.containerDiaryButtons}>
-          <button className={css.diaryButton}>
-            <HourglassIcon className={css.diaryIcon} />
+          <button
+            className={css.diaryButton}
+            onClick={() => setActiveTab("diary")}
+          >
+            <HourglassIcon
+              className={`${css.diaryIcon} ${
+                activeTab === "diary" ? css.diaryIconActive : ""
+              }`}
+            />
           </button>
-          <button className={css.diaryButton}>
-            <PieChartIcon className={css.diaryIcon} />
+
+          <button
+            className={css.diaryButton}
+            onClick={() => setActiveTab("statistics")}
+          >
+            <PieChartIcon
+              className={`${css.diaryIcon} ${
+                activeTab === "statistics" ? css.diaryIconActive : ""
+              }`}
+            />
           </button>
         </div>
       </div>
 
       <div className={css.diaryContentContainer}>
-        <div className={css.diaryContent}>
-          <div className={css.containerProgressInfo}>
-            <div className={css.progressItem}>
-              <p className={css.progressDate}>21.10.2023</p>
-              <p className={css.progressPersent}>7.6%</p>
-              <p className={css.progressMinutes}>29 minutes</p>
-            </div>
-
-            <div className={css.pagesInfo}>
-              <p className={css.pagesAmount}>42 pages</p>
-
-              <div className={css.pagesProgress}>
-                <ProgressIcon className={css.progressIcon} />
-
-                <button className={css.deleteButton} type="button">
-                  <DeleteBtnIcon className={css.deleteIconProgress} />
-                </button>
+        {activeTab === "diary" ? (
+          <div className={css.diaryContent}>
+            <div className={css.containerProgressInfo}>
+              <div className={css.progressItem}>
+                <p className={css.progressDate}>21.10.2023</p>
+                <p className={css.progressPersent}>7.6%</p>
+                <p className={css.progressMinutes}>29 minutes</p>
               </div>
 
-              <div className={css.pagesPerHour}>45 pages per hour</div>
+              <div className={css.pagesInfo}>
+                <p className={css.pagesAmount}>42 pages</p>
+
+                <div className={css.pagesProgress}>
+                  <ProgressIcon className={css.progressIcon} />
+                  <button className={css.deleteButton} type="button">
+                    <DeleteBtnIcon className={css.deleteIconProgress} />
+                  </button>
+                </div>
+
+                <div className={css.pagesPerHour}>45 pages per hour</div>
+              </div>
+            </div>
+
+            <div className={css.containerProgressInfo}>
+              <div className={css.progressItem}>
+                <p className={css.progressDate}>21.10.2023</p>
+                <p className={css.progressPersent}>7.6%</p>
+                <p className={css.progressMinutes}>29 minutes</p>
+              </div>
+
+              <div className={css.pagesInfo}>
+                <p className={css.pagesAmount}>42 pages</p>
+
+                <div className={css.pagesProgress}>
+                  <ProgressIcon className={css.progressIcon} />
+                  <button className={css.deleteButton} type="button">
+                    <DeleteBtnIcon className={css.deleteIconProgress} />
+                  </button>
+                </div>
+
+                <div className={css.pagesPerHour}>45 pages per hour</div>
+              </div>
             </div>
           </div>
-        </div>
+        ) : (
+          <div className={css.statisticProgressContent}>
+            <div className={css.statisticProgressItem}>
+              <p className={css.statisticProgressPersent}>100%</p>
+            </div>
+
+            <div className={css.readingProgressContainer}>
+              <div className={css.marker}></div>
+
+              <div className={css.readingProgress}>
+                <p className={css.progressPersentStat}>0%</p>
+                <p className={css.progressPages}>0 pages read</p>
+              </div>
+            </div>
+          </div>
+        )}
       </div>
     </div>
   );
